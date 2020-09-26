579286c902713fd5a97490da661d6716535b3c84
leonard1on <leonardborj@gmail.com>
2020-09-26 17:00:39 -0600

    Categories and Accounts created

---
+++ backend/models/account.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    money: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Account = mongoose.model("User", accountSchema);
+
+module.exports = Account;
---
+++ backend/routes/accounts.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+let Account = require("../models/account.model");
+
+router.route("/").get((req, res) => {
+  Account.find()
+    .then((accounts) => res.json(accounts))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const userId = req.body.userId;
+  const name = req.body.name;
+  const money = req.body.money;
+  const newAccount = new Account({ userId, name, money });
+
+  newAccount
+    .save()
+    .then(() => res.json("Account added successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Account.findById(req.params.id)
+    .then((account) => res.json(account))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Account.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Account deleted successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Account.findById(req.params.id)
+    .then((account) => {
+      account.userId = req.body.userId;
+      account.name = req.body.name;
+      account.money = req.body.money;
+
+      account
+        .save()
+        .then(() => res.json("Account updated successfully"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
