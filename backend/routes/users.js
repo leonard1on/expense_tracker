3acee2b187af4bba8238c2b23178ae705d7e7115
leonard1on <leonardborj@gmail.com>
2020-09-26 16:00:25 -0600

    Delete, update and getById created

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,7 @@ let User = require("../models/user.model");
 router.route("/").get((req, res) => {
   User.find()
     .then((users) => res.json(users))
-    .catch((err) => res.status(400).json("Error: ", err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/add").post((req, res) => {
@@ -13,8 +13,33 @@ router.route("/add").post((req, res) => {
 
   newUser
     .save()
-    .then(() => res.json("User added succesfully"))
-    .catch((err) => res.status(400).json("Error: ", err));
+    .then(() => res.json("User added successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json("User deleted successfully"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      user.id = req.body.id;
+
+      user
+        .save()
+        .then(() => res.json("User updated successfully"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
