96205a1b18c8f58e7b495e28538a5bcd1a1dc393
leonard1on <leonardborj@gmail.com>
2020-09-26 01:11:04 -0600

    MongoDB Connected

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const PORT = 8080;
@@ -11,6 +12,16 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 
+const uri = process.env.URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB successfully connected");
+});
 app.listen(PORT, () => {
   console.log("Server is running on Port: " + PORT);
 });
